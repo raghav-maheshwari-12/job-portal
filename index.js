5a3e5fd09cd3aaa75e16fdada9e1845e8022e353
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-09-07 20:36:20 +0530

    test api added

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express =require("express");
+const cors =require('cors');
+const morgan =require('morgan');
+const app = express();
+
 const colors =require("colors");
 const dotenv =require("dotenv");
+const  connectDb = require("./config/db");
 
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 dotenv.config();
+connectDb();
 
-//rest object
-const app =express();
 
-//routes
+
+//rest api
+app.use("/api/v1/test",require("./routes/testRoute"));
+
 app.get("/",(req,res)=>{
     res.send("<h1>welcome to my job portal</h1>");
 });
