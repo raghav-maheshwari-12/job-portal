b13bc0fc9068cf5e94a7f0e1d35a7da880975e57
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-09-08 23:30:13 +0530

    auth controller added

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+const userModel = require("../models/userModel");
+
+const registerController =async (req,res,next)=>{
+const { name, email, password } = req.body;
+  //validate
+  if (!name) {
+    next("name is required");
+  }
+  if (!email) {
+    next("email is required");
+  }
+  if (!password) {
+    next("password is required and greater than 6 character");
+  }
+  const exisitingUser = await userModel.findOne({ email });
+  if (exisitingUser) {
+    next("Email Already Register Please Login");
+  }
+  const user = await userModel.create({ name, email, password });
+  //token
+//   const token = user.createJWT();
+  res.status(201).send({
+    sucess: true,
+    message: "User Created Successfully",
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      location: user.location,
+    },
+    // token,
+  });
+};
+
+module.exports ={registerController}
