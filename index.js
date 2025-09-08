345b2e4f26be99822ed5660adf609ae24aea47fb
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-09-08 23:27:53 +0530

    register api added

--- index.js
+++ index.js
@@ -2,27 +2,40 @@ const express =require("express");
 const cors =require('cors');
 const morgan =require('morgan');
 const app = express();
-
 const colors =require("colors");
 const dotenv =require("dotenv");
-const  connectDb = require("./config/db");
 
+//file import
+const  connectDb = require("./config/db");
+const { errroMiddelware } = require("./middlewares/errorMiddleware");
+
+//middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
+
+//dotenv config
 dotenv.config();
 connectDb();
 
 
 
-//rest api
+//routes
 app.use("/api/v1/test",require("./routes/testRoute"));
+app.use("/api/v1/auth",require("./routes/authRoute"));
 
+
+//validation middelware
+app.use(errroMiddelware);
+
+//rest api
 app.get("/",(req,res)=>{
     res.send("<h1>welcome to my job portal</h1>");
 });
+
 //PORT
 const PORT =process.env.PORT || 8080;
+
 //listen
 app.listen(PORT,()=>{
     console.log(`Server runnning on port ${PORT}`.bgBlack.green);
