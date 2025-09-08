1f75c67e15a8c46a0cc68352f6f374d68ff8b174
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-09-08 23:28:31 +0530

    auth route added

---
+++ routes/authRoute.js
@@ -0,0 +1,10 @@
+const express =require("express");
+const { registerController } = require("../controllers/authController");
+
+//router object
+const router = express.Router();
+
+//routes
+router.post("/register",registerController)
+
+module.exports =router;
