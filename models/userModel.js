015b540a93f61bf6b99515beb8f1f436e223b612
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-09-08 23:28:55 +0530

    usermodel added

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose =require("mongoose");
+var validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+//schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name Is Require"],
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, " Email is Require"],
+      unique: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      required: [true, "password is require"],
+      minlength: [6, "Password length should be greater than 6 character"],
+      select: true,
+    },
+    location: {
+      type: String,
+      default: "India",
+    },
+  },
+  { timestamps: true }
+);
+
+//middleware for paawsord hashing
+userSchema.pre("save", async function () {
+  if (!this.isModified) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", userSchema);
