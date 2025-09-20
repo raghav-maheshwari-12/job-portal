0cf47ce08616335a5660d8ae2e3ee68b4fcd7b91
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-09-20 18:19:00 +0530

    job route added

---
+++ routes/jobsRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+//routes
+// CREATE JOB || POST
+router.post("/create-job", userAuth, createJobController);
+
+//GET JOBS || GET
+router.get("/get-job", userAuth, getAllJobsController);
+
+//UPDATE JOBS ||  PATCH
+router.patch("/update-job/:id", userAuth, updateJobController);
+
+//DELETE JOBS || DELETE
+router.delete("/delete-job/:id", userAuth, deleteJobController);
+
+// JOBS STATS FILTER || GET
+router.get("/job-stats", userAuth, jobStatsController);
