73f0c36b86820e23fc9c771ff24d87b577c49130
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-09-24 23:06:06 +0530

    route addedd

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,18 +1,22 @@
 const express = require("express");
+const userAuth = require("../middlewares/authMiddleware");
+const { createJobController, getAllJObControllers } = require("../controllers/jobsController");
 const router = express.Router();
 
 //routes
 // CREATE JOB || POST
 router.post("/create-job", userAuth, createJobController);
 
-//GET JOBS || GET
-router.get("/get-job", userAuth, getAllJobsController);
+// //GET JOBS || GET
+router.get("/get-job", userAuth, getAllJObControllers);
 
-//UPDATE JOBS ||  PATCH
-router.patch("/update-job/:id", userAuth, updateJobController);
+// //UPDATE JOBS ||  PATCH
+// router.patch("/update-job/:id", userAuth, updateJobController);
 
-//DELETE JOBS || DELETE
-router.delete("/delete-job/:id", userAuth, deleteJobController);
+// //DELETE JOBS || DELETE
+// router.delete("/delete-job/:id", userAuth, deleteJobController);
 
-// JOBS STATS FILTER || GET
-router.get("/job-stats", userAuth, jobStatsController);
+// // JOBS STATS FILTER || GET
+// router.get("/job-stats", userAuth, jobStatsController);
+
+module.exports =router;
